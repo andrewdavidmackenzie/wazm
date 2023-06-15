76e7d706af245ecb90c3f21731764cd32f7316d0
Andrew Mackenzie <andrew@mackenzie-serres.net>
2023-06-15 16:30:50 +0200

    Add printing of error

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,6 +1,6 @@
 use std::path::Path;
 use std::fs;
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
 use errors::*;
 
 /// A module with error types
@@ -11,7 +11,7 @@ pub mod errors;
 #[allow(dead_code)] // source not used in lib
 pub struct Analysis {
     source: String,
-    operator_usage: HashMap<String, u64>,
+    operator_usage: BTreeMap<String, u64>,
     operator_count: u64,
     file_size: u64,
 }
@@ -25,7 +25,7 @@ pub fn compress(source: &Path, destination: &Path) -> Result<u64> {
 /// Compress file at `source`into a new file at `destination`
 /// Return a Result with the size of the output file in bytes
 pub fn analyze(source: &Path) -> Result<Analysis> {
-    let mut opcode_usage = HashMap::<String, u64>::new();
+    let mut operator_usage = HashMap::<String, u64>::new();
     let mut operator_count = 0;
 
     let wasm_bytecodes = fs::read(source)?;
@@ -33,18 +33,28 @@ pub fn analyze(source: &Path) -> Result<Analysis> {
     while !reader.eof() {
         let opname = match reader.read_operator() {
             Ok(op) => format!("{:?}", op),
-            Err(_) => "IllegalOpCode".into(), // TODO
+            Err(_) => {
+                eprintln!("Illegal Operation");
+                "IllegalOpCode".into()
+            }, // TODO
         };
-        opcode_usage.entry(opname)
+        operator_usage.entry(opname)
             .and_modify(|count| *count += 1)
             .or_insert(1);
         operator_count += 1;
     }
 
+    let mut vec: Vec<(&String, &u64)> = operator_usage.iter().collect();
+    vec.sort_by(|a, b| b.1.cmp(a.1));
+    let mut sorted_operator_usage = BTreeMap::<String, u64>::new();
+    for (op, count) in vec {
+        sorted_operator_usage.insert(op.to_string(), *count);
+    }
+
     Ok(Analysis {
-        source: source.display().to_string(),
-        operator_usage: opcode_usage,
+        source: source.canonicalize()?.display().to_string(),
+        operator_usage: sorted_operator_usage,
         operator_count,
-        file_size: source.metadata().unwrap().len(),
+        file_size: source.metadata()?.len(),
     })
 }
